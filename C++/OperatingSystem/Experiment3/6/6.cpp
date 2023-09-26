7971d20281704203897a847288cf78f25390c38d
Eric-MiLin-Chen <2479880643@qq.com>
2023-09-26 13:54:02 +0800

    update OS Experiment3 6

---
+++ C++/OperatingSystem/Experiment3/6/6.cpp
@@ -0,0 +1,62 @@
+/*程序建立一个管道fd*/
+/*父进程创建两个子进程P1、P2 */
+/*子进程P1、P2 分别向管道写入信息*/
+/*父进程等待子进程结束，并读出管道中的信息*/
+#include <iostream>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main()
+{
+    int i, r, j, k, l, p1, p2, fd[2];
+    char buf[50], s[50];
+    int res = pipe(fd);         /*建立一个管道fd*/
+    while ((p1 = fork()) == -1) /*创建子进程1*/
+        ;
+    if (p1 == 0) /*子进程1执行*/
+    {
+        res = lockf(fd[1], 1, 0); /*管道写入端加锁*/
+        sprintf(buf, "Child process P1 is sending messages! \n");
+        printf("Child process P1! \n");
+        res = write(fd[1], buf, 50); /*信息写入管道*/
+        res = lockf(fd[1], 0, 0);    /*管道写入端解锁*/
+        sleep(5);
+        j = getpid();
+        k = getppid();
+        printf("P1 %d is weakup. My parent process ID is %d.\n", j, k);
+        exit(0);
+    }
+    else
+    {
+        while ((p2 = fork()) == -1) /*创建子进程2*/
+            ;
+        if (p2 == 0) /*子进程2执行*/
+        {
+            res = lockf(fd[1], 1, 0); /*管道写入端加锁*/
+            sprintf(buf, "Child process P2 is sending messages! \n");
+            printf("Child process P2! \n");
+            res = write(fd[1], buf, 50); /*信息写入管道*/
+            res = lockf(fd[1], 0, 0);    /*管道写入端解锁*/
+            sleep(5);
+            j = getpid();
+            k = getppid();
+            printf("P2 %d is weakup. My parent process ID is %d.\n", j, k);
+            exit(0);
+        }
+        else
+        {
+            l = getpid();
+            wait(0); /* 等待被唤醒*/
+            if (r = read(fd[0], s, 50) == -1)
+                printf("Can't read pipe. \n");
+            else
+                printf("Parent %d: %s \n", l, s);
+            wait(0); /* 等待被唤醒*/
+            if (r = read(fd[0], s, 50) == -1)
+                printf("Can't read pipe. \n");
+            else
+                printf("Parent %d: %s \n", l, s);
+            exit(0);
+        }
+    }
+}
